26d4c29580acb7d69289dbc6573e7a8d982419fc
satya <satyaspd12@gmail.com>
2022-10-30 13:05:54 +0530

    adding tabs

--- app/src/main/java/com/satya/mealbox/ui/activity/MainActivity.kt
+++ app/src/main/java/com/satya/mealbox/ui/activity/MainActivity.kt
@@ -50,7 +50,10 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home
+                R.id.nav_home,
+                R.id.nav_meal_planning,
+                R.id.nav_subscription,
+                R.id.nav_profile
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
